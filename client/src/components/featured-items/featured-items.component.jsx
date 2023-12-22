ecd990c708da7599199de2a2469ee7885efd64f4
oscarluna <oscarluna.webdev@gmail.com>
2023-12-22 11:02:30 -0800

    Fix mobile layout errors part 2

--- client/src/components/featured-items/featured-items.component.jsx
+++ client/src/components/featured-items/featured-items.component.jsx
@@ -1,182 +1,163 @@
-import {useState} from "react";
-import {AnimatePresence, motion} from "framer-motion";
-import {FEATURED_ITEM_DATA} from "../../pages/shop/shop.data"
-import {ArrowLeftIcon, ArrowRightIcon} from "@heroicons/react/24/solid";
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/24/solid";
 
-const featuredItemData = [{
-
-    image: `${FEATURED_ITEM_DATA[0].imageUrl}`,
-    name: `${FEATURED_ITEM_DATA[0].name}`,
-    price: `${FEATURED_ITEM_DATA[0].pricingText}`
-}, {
-
-    image: `${FEATURED_ITEM_DATA[1].imageUrl}`,
-    name: `${FEATURED_ITEM_DATA[1].name}`,
-    price: `${FEATURED_ITEM_DATA[1].pricingText}`
-}, {
-
-    image: `${FEATURED_ITEM_DATA[2].imageUrl}`,
-    name: `${FEATURED_ITEM_DATA[2].name}`,
-    price: `${FEATURED_ITEM_DATA[2].pricingText}`
-},
-    {
-
-        image: `${FEATURED_ITEM_DATA[3].imageUrl}`,
-        name: `${FEATURED_ITEM_DATA[3].name}`,
-        price: `${FEATURED_ITEM_DATA[3].pricingText}`
-    },
-    {
-
-        image: `${FEATURED_ITEM_DATA[4].imageUrl}`,
-        name: `${FEATURED_ITEM_DATA[4].name}`,
-        price: `${FEATURED_ITEM_DATA[4].pricingText}`
-    }];
 const images = [
-    "https://res.cloudinary.com/dgdnpkfun/image/upload/v1673493607/arrangements/brightenDay_fanxij.png",
-    "https://res.cloudinary.com/dgdnpkfun/image/upload/v1656003850/arrangements/cosmoplolitan-design-alt_pmwds2.png",
-    "https://res.cloudinary.com/dgdnpkfun/image/upload/w_500,h_550/v1656003850/arrangements/metropolitan-design_fashox.png",
-    "https://res.cloudinary.com/dgdnpkfun/image/upload/v1657057747/arrangements/massive-metropolitan_jyj0ht.png",
-    "https://res.cloudinary.com/dgdnpkfun/image/upload/v1656003846/arrangements/asymmetric-centerpiece-design_anphoq.png"
+  "https://res.cloudinary.com/dgdnpkfun/image/upload/v1673493607/arrangements/brightenDay_fanxij.png",
+  "https://res.cloudinary.com/dgdnpkfun/image/upload/v1656003850/arrangements/cosmoplolitan-design-alt_pmwds2.png",
+  "https://res.cloudinary.com/dgdnpkfun/image/upload/w_500,h_550/v1656003850/arrangements/metropolitan-design_fashox.png",
+  "https://res.cloudinary.com/dgdnpkfun/image/upload/v1657057747/arrangements/massive-metropolitan_jyj0ht.png",
+  "https://res.cloudinary.com/dgdnpkfun/image/upload/v1656003846/arrangements/asymmetric-centerpiece-design_anphoq.png",
 ];
 
-
-
 const FeaturedItems = () => {
-    const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-    const [direction, setDirection] = useState(null);
+  const [direction, setDirection] = useState(null);
 
-    const slideVariants = {
-        hiddenRight: {
-            x: "50%", opacity: 0,
-        }, hiddenLeft: {
-            x: "-100%", opacity: 0,
-        }, visible: {
-            x: "0", opacity: 1, transition: {
-                duration: 1,
-            }, hover: {
-                scale: 1.2, transition: {
-                    duration: 0.75,
-                },
-            },
-        }, exit: {
-            x: "-100%", opacity: 0, scale: 0.8, transition: {
-                duration: 0.5,
-            },
+  const slideVariants = {
+    hiddenRight: {
+      x: "50%",
+      opacity: 0,
+    },
+    hiddenLeft: {
+      x: "-100%",
+      opacity: 0,
+    },
+    visible: {
+      x: "0",
+      opacity: 1,
+      transition: {
+        duration: 1,
+      },
+      hover: {
+        scale: 1.2,
+        transition: {
+          duration: 0.75,
         },
-    };
+      },
+    },
+    exit: {
+      x: "-100%",
+      opacity: 0,
+      scale: 0.8,
+      transition: {
+        duration: 0.5,
+      },
+    },
+  };
 
-    const slidersVariants = {
-        hover: {
-            backgroundColor: "#ff00008e",
-        },
-    };
+  const slidersVariants = {
+    hover: {
+      backgroundColor: "#ff00008e",
+    },
+  };
 
-    const dotsVariants = {
-        initial: {
-            y: 0,
-        }, animate: {
-            y: 0, scale: 1.2, transition: {type: "spring", stiffness: 1000, damping: "10"},
-        }, hover: {
-            scale: 1.1, transition: {duration: 0.2},
-        },
-    };
+  const dotsVariants = {
+    initial: {
+      y: 0,
+    },
+    animate: {
+      y: 0,
+      scale: 1.2,
+      transition: { type: "spring", stiffness: 1000, damping: "10" },
+    },
+    hover: {
+      scale: 1.1,
+      transition: { duration: 0.2 },
+    },
+  };
 
-    const handleNext = () => {
-        setDirection("right");
-        setCurrentIndex((prevIndex) => prevIndex + 1 === images.length ? 0 : prevIndex + 1);
-    };
+  const handleNext = () => {
+    setDirection("right");
+    setCurrentIndex((prevIndex) =>
+      prevIndex + 1 === images.length ? 0 : prevIndex + 1
+    );
+  };
 
-    const handlePrevious = () => {
-        setDirection("left");
-        setCurrentIndex((prevIndex) => prevIndex - 1 < 0 ? images.length - 1 : prevIndex - 1);
-    };
+  const handlePrevious = () => {
+    setDirection("left");
+    setCurrentIndex((prevIndex) =>
+      prevIndex - 1 < 0 ? images.length - 1 : prevIndex - 1
+    );
+  };
 
-    const handleDotClick = (index) => {
-        setDirection(index > currentIndex ? "right" : "left");
-        setCurrentIndex(index);
-    };
+  const handleDotClick = (index) => {
+    setDirection(index > currentIndex ? "right" : "left");
+    setCurrentIndex(index);
+  };
 
-    return (<div className="carousel">
-            <div
-                className="relative rounded-lg h-full lg:h-96 max-w-1/4 mx-auto mt-0 lg:mt-48 lg:mb-12 overflow-hidden hover:overflow-visible">
-                <AnimatePresence mode="wait">
-                    <motion.img
-                        key={currentIndex}
-                        src={images[currentIndex]}
-                        initial={direction === "right" ? "hiddenRight" : "hiddenLeft"}
-                        animate="visible"
-                        exit="exit"
-                        variants={slideVariants}
-                        className="object-contain w-[400px] h-[300px] rounded-md border-none"
-                    />
-                </AnimatePresence>
-                <div className="flex justify-between">
-                    <motion.div
-                        variants={slidersVariants}
-                        whileHover="hover"
-                        className="left"
-                        onClick={handlePrevious}
-                    >
-                        <ArrowLeftIcon
-                            className="bg-amber-500/50 text-white p-2 rounded-full absolute top-1/2 left-0 transform -translate-y-1/2 w-12 h-12"/>
-                    </motion.div>
-                    <motion.div
-                        variants={slidersVariants}
-                        whileHover="hover"
-                        className="right"
-                        onClick={handleNext}
-                    >
-                        <ArrowRightIcon
-                            className="bg-amber-500/50 text-white p-2 rounded-full absolute top-1/2 right-0 transform -translate-y-1/2 w-12 h-12"/>
-                    </motion.div>
-                </div>
-            </div>
+  return (
+    <div className="carousel">
+      <div className="relative rounded-lg h-full lg:h-96 max-w-1/4 mx-auto mt-0 lg:mt-48 lg:mb-12 overflow-hidden hover:overflow-visible">
+        <AnimatePresence mode="wait">
+          <motion.img
+            key={currentIndex}
+            src={images[currentIndex]}
+            initial={direction === "right" ? "hiddenRight" : "hiddenLeft"}
+            animate="visible"
+            exit="exit"
+            variants={slideVariants}
+            className="object-contain w-[400px] h-[300px] rounded-md border-none"
+          />
+        </AnimatePresence>
+        <div className="flex justify-between">
+          <motion.div
+            variants={slidersVariants}
+            whileHover="hover"
+            className="left"
+            onClick={handlePrevious}
+          >
+            <ArrowLeftIcon className="bg-amber-500/50 text-white p-2 rounded-full absolute top-1/2 left-0 transform -translate-y-1/2 w-12 h-12" />
+          </motion.div>
+          <motion.div
+            variants={slidersVariants}
+            whileHover="hover"
+            className="right"
+            onClick={handleNext}
+          >
+            <ArrowRightIcon className="bg-amber-500/50 text-white p-2 rounded-full absolute top-1/2 right-0 transform -translate-y-1/2 w-12 h-12" />
+          </motion.div>
+        </div>
+      </div>
 
-            <div className="mt-8 flex justify-center gap-8">
-                {images.map((_, index) => (<motion.div
-                        key={index}
-                        className={`dot ${currentIndex === index ? "w-4 h-4 bg-amber-500 rounded-full" : "w-4 h-4 bg-gray-700 rounded-full"}`}
-                        onClick={() => handleDotClick(index)}
-                        initial="initial"
-                        animate={currentIndex === index ? "animate" : ""}
-                        whileHover="hover"
-                        variants={dotsVariants}
-                    ></motion.div>))}
-            </div>
-
-        </div>);
+      <div className="mt-8 flex justify-center gap-8">
+        {images.map((_, index) => (
+          <motion.div
+            key={index}
+            className={`dot ${
+              currentIndex === index
+                ? "w-4 h-4 bg-amber-500 rounded-full"
+                : "w-4 h-4 bg-gray-700 rounded-full"
+            }`}
+            onClick={() => handleDotClick(index)}
+            initial="initial"
+            animate={currentIndex === index ? "animate" : ""}
+            whileHover="hover"
+            variants={dotsVariants}
+          ></motion.div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
-const FeaturedItemsData = ({currentIndex, featuredItemData}) => (
-    <div className="mt-2 w-full h-auto flex justify-center gap-1 z-100 relative">
-        {featuredItemData.map((item, index) => (
-            <>
-                <div className="mt-36">
-                    <h2 className="z-100 text-xl md:text-5xl">{featuredItemData[index] === currentIndex && item.name}</h2>
-                </div></>
-        ))}
+
+const FeaturedItemsContainer = () => (
+  <div className="flex flex-col md:flex-row md:items-center md:justify-evenly md:mx-auto md:w-screen my-24 md:my-32">
+    <div className="text-left lg:my-12 mx-5">
+      <h1 className="text-7xl font-light font-[Urbanist]">
+        Our Featured Items
+      </h1>
+      <div className="text-md font-light font-[Urbanist]">
+        <p>Browse Premium Selections in Our Featured Assortment</p>
+      </div>
     </div>
-)
-
-
-const FeaturedItemsContainer = () => (<>
-        <div className="text-left">
-            <h1 className="text-7xl font-medium font-[Urbanist] m-5">
-                Our Featured Items
-            </h1>
-        </div>
-        <div className="text-md font-light font-[Urbanist] m-5">
-            <p>Browse Premium Selections in Our Featured Assortment</p>
-        </div>
-        <div
-            className="w-screen h-[100vh] space-y-1 flex flex-col lg:flex-row items-center justify-evenly bg-gradient-to-tr from-rose-200 to-amber-200 rounded-lg shadow-xl mt-0 lg:py-[32vh] md:py-72 relative">
-
-            <div className="h-full">
-                <FeaturedItemsData featuredItemData={featuredItemData} />
-
-                <FeaturedItems images={images} />
-            </div>
-        </div>
-    </>);
+    <div className="w-screen md:w-1/2 h-[100vh] md:h-1/2 lg:h-[80vh] space-y-1 md:space-y-0 flex flex-col md:flex-row items-center md:items-start justify-evenly lg:justify-between bg-gradient-to-tr from-rose-200 to-amber-200 rounded-lg shadow-xl mt-0 py-32 md:py-0 relative">
+      <div className="mx-auto h-full md:h-[50%] md:h-auto md:mt-[0vh] flex flex-row items-center justify-between">
+        <FeaturedItems images={images} />
+      </div>
+    </div>
+  </div>
+);
 export default FeaturedItemsContainer;
--- client/src/components/hero/hero.component.jsx
+++ client/src/components/hero/hero.component.jsx
@@ -11,28 +11,28 @@ const Hero = () => {
       <img src={MeshGradient} className="p-0 absolute right-0  max-w-full w-1/2 h-screen object-cover overflow-hidden mix-blend-hue-rotate  bg-clip-content" alt=""/>
       <div className="w-full p-0 m-0 flex h-full items-center">
         <div className="p-0 m-0 z-10 w-screen mx-auto text-center text-white bg-transparent bg-clip-text mix-blend-exclusion tracking-none leading-none">
-          <div className="flex flex-row items-center w-screen lg:h-screen justify-around h-1/2 md:h-full">
-            <h1 className="text-5xl md:text-6xl lg:text-7xl font-thin font-[Urbanist] uppercase text-white z-0 w-full">
+          <div className="flex flex-row items-center w-screen h-screen lg:h-auto justify-around lg:mt-[20%]">
+            <h1 className="text-4xl md:text-6xl lg:text-[160px] font-thin font-[Urbanist] uppercase text-white z-0 w-full">
               Glowing
             </h1>
 
-            <h1 className="text-5xl md:text-6xl lg:text-7xl font-thin font-[Urbanist] uppercase text-white z-0 w-full">
+            <h1 className="text-4xl md:text-6xl lg:text-[160px] font-thin font-[Urbanist] uppercase text-white z-0 w-full">
               Flowers
             </h1>
           </div>
-          <p className="font-[Urbanist] text-4xl font-light tracking-[1px] sm:block z-0 my-0">
+          <p className="absolute md:relative top-[55%] left-auto right-auto w-full font-[Urbanist] text-xl md:text-4xl font-light tracking-[1px] sm:block z-0 my-0 lg:mb-[20%] lg:mt-0 lg:h-[40px]">
             {" "}
             Plants, Gifts & Balloons{" "}
           </p>
         </div>
       </div>
-      <div className="absolute w-screen mx-auto flex-col bottom-60 text-center items-center h-24 md:h-48 md:bottom:40">
-        <p className="text-white  max-w-xl sm:text-xl/relaxed font-normal tracking-[1.25px] font-[Urbanist mix-blend-normal mx-auto">
+      <div className="absolute w-screen mx-auto flex-col bottom-20 text-center items-center h-24 md:h-48 md:bottom:40 lg:bottom-12">
+        <p className="text-white  max-w-xl sm:text-xl/relaxed font-normal tracking-tight md:tracking-[1.25px] font-[Urbanist mix-blend-normal mx-auto leading-tight">
           Simple and beautiful floral arrangements, located in the heart of San
           Francisco.
         </p>
       </div>
-      <div className="absolute z-10 flex flex-row flex-wrap items-start justify-center w-screen h-auto gap-8 py-8 top-[75%]">
+      <div className="absolute z-10 flex flex-row flex-wrap items-start justify-center w-screen h-auto gap-1 md:gap-8 py-8 top-[72%]">
         <a
           className="block min-w-[220px] rounded border border-slate-50 bg-transparent px-12 py-3 text-2xl font-medium text-slate-50 mix-blend-difference hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-opacity-75 text-center font-[Urbanist]"
           href="/shop"
